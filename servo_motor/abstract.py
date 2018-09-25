c0971f5e728478d7fcb3bb8af74d75a0be4fae9a
SaltyHash <austin.bowen.314@gmail.com>
2018-09-25 15:00:49 -0400

    First commit

---
+++ servo_motor/abstract.py
@@ -0,0 +1,98 @@
+def scale(x, point0, point1):
+    x0, y0 = point0
+    x1, y1 = point1
+    return (x - x0) * (y1 - y0) / (x1 - x0) + y0
+
+
+class ServoMotorController:
+    def set_pwm_us(self, pwm_us):
+        pass
+
+
+class ServoMotor:
+    __slots__ = (
+        'controller',
+        'full_cw_pwm_us',
+        'full_ccw_pwm_us'
+    )
+
+    def __init__(
+            self,
+            controller: ServoMotorController,
+            full_cw_pwm_us,
+            full_ccw_pwm_us
+    ):
+        self.controller = controller
+        self.full_cw_pwm_us = full_cw_pwm_us
+        self.full_ccw_pwm_us = full_ccw_pwm_us
+
+
+class LimitedServoMotor(ServoMotor):
+    __slots__ = (
+        'full_cw_deg',
+        'full_ccw_deg',
+    )
+
+    def __init__(
+            self,
+            controller: ServoMotorController,
+            full_cw_deg,
+            full_cw_pwm_us,
+            full_ccw_deg,
+            full_ccw_pwm_us
+    ):
+        ServoMotor.__init__(self, controller, full_cw_pwm_us, full_ccw_pwm_us)
+        self.full_cw_deg = full_cw_deg
+        self.full_ccw_deg = full_ccw_deg
+
+    def set_angle(self, deg):
+        pwm_us = scale(
+            deg,
+            (self.full_cw_deg, self.full_cw_pwm_us),
+            (self.full_ccw_deg, self.full_ccw_pwm_us)
+        )
+        self.controller.set_pwm_us(pwm_us)
+
+
+class ContinuousServoMotor(ServoMotor):
+    __slots__ = (
+        'center_pwm_us',
+        'hard_max_rpm'
+    )
+
+    def __init__(
+            self,
+            controller: ServoMotorController,
+            hard_max_rpm,
+            center_pwm_us,
+            full_cw_pwm_us,
+            full_ccw_pwm_us
+    ):
+        ServoMotor.__init__(self, controller, full_cw_pwm_us, full_ccw_pwm_us)
+        self.hard_max_rpm = hard_max_rpm
+        self.center_pwm_us = center_pwm_us
+
+    def map_speed_to_pwm_us(self, speed):
+        """
+        Continuous rotation servos may have a non-linear speed response to the PWM signal.
+        This method should convert the speed into PWM values such that the servo speed
+        varies linearly as the speed varies linearly.
+
+        By default, this method simply scales speeds from (0, 1] to (center_pwm_us, full_cw_pwm_us],
+        and speeds from [-1, 0) to [full_ccw_pwm_us, center_pwm_us).
+
+        :param speed: Range [-1, 1], where -1 corresponds to full CCW, and +1 corresponds to full CW.
+        """
+        if speed > 0:
+            return scale(speed, (0, self.center_pwm_us), (1, self.full_cw_pwm_us))
+        elif speed < 0:
+            return scale(speed, (0, self.center_pwm_us), (-1, self.full_ccw_pwm_us))
+        else:
+            return self.center_pwm_us
+
+    def set_rpm(self, rpm):
+        speed = scale(rpm, (0, 0), (self.hard_max_rpm, 1))
+        self.set_speed(speed)
+
+    def set_speed(self, speed):
+        self.controller.set_pwm_us(self.map_speed_to_pwm_us(speed))
---
+++ servo_motor/model.py
@@ -0,0 +1,37 @@
+from .abstract import *
+
+
+class ParallaxHighSpeedContinuousServoMotor(ContinuousServoMotor):
+    def __init__(self, controller: ServoMotorController, voltage: float):
+        hard_max_rpm = scale(voltage, (6, 150), (7.4, 180))
+        ContinuousServoMotor.__init__(
+            self,
+            controller,
+            hard_max_rpm,
+            center_pwm_us=1500,
+            full_cw_pwm_us=1300,
+            full_ccw_pwm_us=1700
+        )
+
+    def map_speed_to_pwm_us(self, speed):
+        """
+        The map of PWM signal to speed for this servo looks like a sigmoid function.  See page 3 of the documentation:
+        https://www.parallax.com/sites/default/files/downloads/900-00025-High-Speed-CR-Servo-Guide-v1.1.pdf
+        """
+        # TODO: Account for dead zone?
+        full_cw_point = (1, self.full_cw_pwm_us)
+        near_full_cw_point = (0.88, 1420)
+        center_point = (0, self.center_pwm_us)
+        near_full_ccw_point = (-0.88, 1580)
+        full_ccw_point = (-1, self.full_ccw_pwm_us)
+
+        if speed >= near_full_cw_point[0]:
+            return scale(speed, near_full_cw_point, full_cw_point)
+        elif speed > 0:
+            return scale(speed, center_point, near_full_cw_point)
+        elif speed == 0:
+            return self.center_pwm_us
+        elif speed >= near_full_ccw_point[0]:
+            return scale(speed, center_point, near_full_ccw_point)
+        else:
+            return scale(speed, near_full_ccw_point, full_ccw_point)
