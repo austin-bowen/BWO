10389dc69c7cd17c3dafa9fd58f03b3b18cdad7d
SaltyHash <austin.bowen.314@gmail.com>
2018-09-29 23:10:01 -0400

    Add keyboard control of driving.

--- bwo/io/motor.py
+++ bwo/io/motor.py
@@ -11,10 +11,13 @@ Number = Union[float, int]
 
 
 class DriveMotorController:
-    _body_to_wheel_velocity = np.array((
+    SPEED = 8
+    ACCELERATION = 0
+
+    _BODY_TO_WHEEL_VELOCITY = np.array((
         (1, 0, -settings.BODY_RADIUS.meters),
-        (-0.5, np.sqrt(3), -settings.BODY_RADIUS.meters),
-        (-0.5, -np.sqrt(3), -settings.BODY_RADIUS.meters)
+        (-0.5, np.sqrt(3) / 2, -settings.BODY_RADIUS.meters),
+        (-0.5, -np.sqrt(3) / 2, -settings.BODY_RADIUS.meters)
     ))
 
     class Motor(Enum):
@@ -29,8 +32,17 @@ class DriveMotorController:
 
     def __init__(self, maestro: Maestro):
         self.maestro = maestro
+        channels = (
+            self.Motor.LEFT.get_channel(),
+            self.Motor.RIGHT.get_channel(),
+            self.Motor.BACK.get_channel()
+        )
+        for channel in channels:
+            maestro.set_speed(channel, self.SPEED)
+            maestro.set_acceleration(channel, self.ACCELERATION)
+
         self.motor_model = ParallaxHighSpeedContinuousServoMotor(settings.MOTOR_VOLTAGE)
-        # self.stop()
+        self.stop()
 
         self.max_wheel_speed_mps = settings.WHEEL_CIRCUMFERENCE.meters * self.motor_model.hard_max_rpm / 60
         print('Max wheel speed [m/s]:', round(self.max_wheel_speed_mps, 2))
@@ -41,11 +53,21 @@ class DriveMotorController:
             self.motor_model.get_pwm_us_for_rpm(rpm)
         )
 
+    def set_motor_rpms(self, left_rpm: Number, right_rpm: Number, back_rpm: Number):
+        self.set_motor_rpm(self.Motor.LEFT, left_rpm)
+        self.set_motor_rpm(self.Motor.RIGHT, right_rpm)
+        self.set_motor_rpm(self.Motor.BACK, back_rpm)
+
     def set_motor_speed(self, motor: Motor, speed: Number):
         self.maestro.set_target(
             motor.get_channel(),
             self.motor_model.get_pwm_us_for_speed(speed)
         )
+
+    def set_motor_speeds(self, left_speed: Number, right_speed: Number, back_speed: Number):
+        self.set_motor_speed(self.Motor.LEFT, left_speed)
+        self.set_motor_speed(self.Motor.RIGHT, right_speed)
+        self.set_motor_speed(self.Motor.BACK, back_speed)
 
     def set_body_velocity(self, v_x: Number, v_y: Number, omega: Number):
         """
@@ -63,28 +85,33 @@ class DriveMotorController:
             (np.deg2rad(omega),)
         ))
 
-        wheel_velocity = np.matmul(self._body_to_wheel_velocity, body_velocity)
+        wheel_velocity = np.matmul(self._BODY_TO_WHEEL_VELOCITY, body_velocity)
         back_wheel_velocity, right_wheel_velocity, left_wheel_velocity = wheel_velocity[:, 0]
         print('left :', left_wheel_velocity)
         print('right:', right_wheel_velocity)
         print('back :', back_wheel_velocity)
 
-        motor_to_wheel_velocity = {
-            self.Motor.LEFT: left_wheel_velocity,
-            self.Motor.RIGHT: right_wheel_velocity,
-            self.Motor.BACK: back_wheel_velocity
-        }
+        motor_rpms = np.array((
+            self.wheel_velocity_to_motor_rpm(left_wheel_velocity),
+            self.wheel_velocity_to_motor_rpm(right_wheel_velocity),
+            self.wheel_velocity_to_motor_rpm(back_wheel_velocity)
+        ))
+        print(motor_rpms)
 
-        if True:
-            for motor, wheel_velocity in motor_to_wheel_velocity.items():
-                self.set_motor_rpm(motor, self.wheel_velocity_to_motor_rpm(wheel_velocity))
+        max_motor_rpm = np.abs(motor_rpms).max()
+        if max_motor_rpm > self.motor_model.hard_max_rpm:
+            print('WARNING: Commanded to move motor(s) faster than their max RPM; scaling down.')
+            motor_rpms = motor_rpms * self.motor_model.hard_max_rpm / max_motor_rpm
+            print(motor_rpms)
+
+        self.set_motor_rpms(*motor_rpms)
 
     def stop(self):
         for motor in DriveMotorController.Motor:
             self.stop_motor(motor)
 
     def stop_motor(self, motor: Motor):
-        self.maestro.stop_channel(motor.get_channel())
+        self.set_motor_rpm(motor, 0)
 
     @staticmethod
     def wheel_velocity_to_motor_rpm(v_wheel: Number):
--- bwo/main.py
+++ bwo/main.py
@@ -2,8 +2,97 @@
 BWO the robot.
 """
 
-from maestro import Maestro
+from collections import namedtuple
+from math import sqrt
+
+from pynput import keyboard
+
 from bwo.io.motor import DriveMotorController
+from maestro import Maestro
+
+prev_velocity = None
+
+
+def with_maestro(maestro: Maestro):
+    FORWARD_KEY = 'w'
+    BACKWARD_KEY = 's'
+    LEFT_KEY = keyboard.Key.left
+    RIGHT_KEY = keyboard.Key.right
+    ROTATE_CW_KEY = 'd'
+    ROTATE_CCW_KEY = 'a'
+
+    XY_MAGNITUDE = 0.3
+    OMEGA_MAGNITUDE = 90
+
+    Velocity = namedtuple('Velocity', ('x', 'y', 'omega'))
+
+    global prev_velocity
+    prev_velocity = Velocity(0.0, 0.0, 0.0)
+
+    motor_controller = DriveMotorController(maestro)
+
+    def update_velocity(x, y, omega):
+        print(x, y, omega)
+        global prev_velocity
+        new_velocity = Velocity(x, y, omega)
+
+        if new_velocity == prev_velocity:
+            print('No need to update velocity')
+
+        print('New velocity:', new_velocity)
+        if x and y:
+            x *= 0.87
+            y *= 0.87
+        motor_controller.set_body_velocity(x, y, omega)
+
+        prev_velocity = new_velocity
+
+    def on_key_press(key):
+        global prev_velocity
+
+        try:
+            key = key.char
+        except AttributeError:
+            pass
+
+        if key == FORWARD_KEY:
+            print('FORWARD')
+            update_velocity(prev_velocity.x, XY_MAGNITUDE, prev_velocity.omega)
+        elif key == BACKWARD_KEY:
+            print('BACKWARD')
+            update_velocity(prev_velocity.x, -XY_MAGNITUDE, prev_velocity.omega)
+        elif key == LEFT_KEY:
+            print('LEFT')
+            update_velocity(-XY_MAGNITUDE, prev_velocity.y, prev_velocity.omega)
+        elif key == RIGHT_KEY:
+            print('RIGHT')
+            update_velocity(XY_MAGNITUDE, prev_velocity.y, prev_velocity.omega)
+        elif key == ROTATE_CW_KEY:
+            print('CW')
+            update_velocity(prev_velocity.x, prev_velocity.y, OMEGA_MAGNITUDE)
+        elif key == ROTATE_CCW_KEY:
+            print('CCW')
+            update_velocity(prev_velocity.x, prev_velocity.y, -OMEGA_MAGNITUDE)
+        elif key == keyboard.Key.esc:
+            return False
+
+    def on_key_release(key):
+        global prev_velocity
+
+        try:
+            key = key.char
+        except AttributeError:
+            pass
+
+        if key == FORWARD_KEY or key == BACKWARD_KEY:
+            update_velocity(prev_velocity.x, 0.0, prev_velocity.omega)
+        elif key == LEFT_KEY or key == RIGHT_KEY:
+            update_velocity(0.0, prev_velocity.y, prev_velocity.omega)
+        elif key == ROTATE_CW_KEY or key == ROTATE_CCW_KEY:
+            update_velocity(prev_velocity.x, prev_velocity.y, 0.0)
+
+    with keyboard.Listener(on_press=on_key_press, on_release=on_key_release) as keyboard_listener:
+        keyboard_listener.join()
 
 
 def main():
@@ -12,18 +101,7 @@ def main():
 
         print('Max RPM: {}\n'.format(drive_motors.motor_model.hard_max_rpm))
 
-        while True:
-            try:
-                v_x, v_y, omega = input('v_x, v_y, omega = ').split(',')
-                v_x = float(v_x.strip())
-                v_y = float(v_y.strip())
-                omega = float(omega.strip())
-            except:
-                break
-
-            drive_motors.set_body_velocity(v_x, v_y, omega)
-
-        drive_motors.stop()
+        with_maestro(maestro)
 
 
 if __name__ == '__main__':
