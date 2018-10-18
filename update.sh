20f649eed715771a0889d5441b958feae43a49a6
SaltyHash <austin.bowen.314@gmail.com>
2018-10-17 21:13:29 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-
-
 echo Pulling changes...
-wget https://raw.githubusercontent.com/SaltyHash/PycoTTS/master/pycotts.py
 git pull
+wget https://raw.githubusercontent.com/SaltyHash/PycoTTS/master/pycotts.py
 
 echo Installing dependencies...
-pip3 install -r requirements.txt
-
-echo Starting BWO!
-python3 bwo.py
+pipenv install -r requirements.txt
