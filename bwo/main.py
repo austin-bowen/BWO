6bf5a34b85eeda5636de74a01a496e2f1c69ee59
SaltyHash <austin.bowen.314@gmail.com>
2018-11-09 23:59:37 -0500

    Add "cat seeker" mode.

--- bwo/main.py
+++ bwo/main.py
@@ -2,13 +2,101 @@
 BWO the robot.
 """
 
+from enum import Enum
 from numbers import Real
 from time import sleep, time
+
+import cv2
+import numpy as np
 
 import gamesir
 from bwo.io.head import Head
 from bwo.io.motor import DriveMotorController
 from maestro import Maestro
+
+
+class BwoMode(Enum):
+    REMOTE_CONTROL = 1
+    CAT_SEEKER = 2
+
+
+MODE = BwoMode.CAT_SEEKER
+
+
+class Bwo:
+    def __enter__(self):
+        self.maestro = Maestro(is_micro=True)
+        self.head = Head(self.maestro)
+        self.drive_motors = DriveMotorController(self.maestro)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        with self.drive_motors, self.maestro:
+            pass
+
+
+class VideoCapture(cv2.VideoCapture):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.release()
+
+
+def cat_seeker(bwo: Bwo, controller: gamesir.GameSirController):
+    cat_detector = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+
+    blackout_start_time = None
+    with VideoCapture(0) as camera:
+        while True:
+            # Get frame
+            ret, frame = camera.read()
+
+            # Blackout frame?
+            if np.max(frame) <= 20:
+                now = time()
+                if blackout_start_time is None:
+                    blackout_start_time = now
+                    bwo.drive_motors.stop()
+                    continue
+                elif (now - blackout_start_time) >= 10:
+                    print('Blackout timeout reached!')
+                    break
+            else:
+                blackout_start_time = None
+
+            # Detect cats
+            cat_rects = cat_detector.detectMultiScale(
+                frame,
+                scaleFactor=1.1,
+                minNeighbors=1,
+                minSize=(10, 10)
+            )
+            if not len(cat_rects):
+                continue
+
+            # Pick the largest detected cat
+            cat_rect = max(cat_rects, key=lambda rect: rect[2] * rect[3])
+            del cat_rects
+
+            # Get the cat's center point
+            cat_center = (
+                cat_rect[0] + (cat_rect[2] / 2),
+                cat_rect[1] + (cat_rect[3] / 2)
+            )
+            del cat_rect
+
+            # Get the cat's offset from center
+            frame_height, frame_width, _ = frame.shape
+            cat_offset = (
+                2 * (cat_center[0] / frame_width) - 1,
+                2 * (cat_center[1] / frame_height) - 1
+            )
+            del cat_center, frame_height, frame_width
+
+            # Set the body velocity and head position
+            bwo.drive_motors.set_body_velocity(0, 0.5, cat_offset[0] * 0.5)
+            bwo.head.set_head_position(0)
 
 
 def get_controller(timeout: Real = 60) -> gamesir.GameSirController:
@@ -26,7 +114,7 @@ def get_controller(timeout: Real = 60) -> gamesir.GameSirController:
         sleep(1)
 
 
-def main(controller: gamesir.GameSirController, maestro: Maestro):
+def remote_control(bwo: Bwo, controller: gamesir.GameSirController):
     drive_motor_events = {
         controller.EventCode.LEFT_JOYSTICK_X,
         controller.EventCode.LEFT_JOYSTICK_Y,
@@ -46,34 +134,38 @@ def main(controller: gamesir.GameSirController, maestro: Maestro):
 
     velocity = Velocity(0, 0, 0)
 
-    head = Head(maestro)
+    for event in controller.read_loop():
+        if event.type not in controller.EVENT_TYPES:
+            continue
 
-    with DriveMotorController(maestro) as drive_motors:
-        for event in controller.read_loop():
-            if event.type not in controller.EVENT_TYPES:
-                continue
+        event_code = controller.EventCode(event.code)
 
-            event_code = controller.EventCode(event.code)
+        if event_code in drive_motor_events:
+            if event_code == controller.EventCode.LEFT_JOYSTICK_X:
+                velocity.x = event.value / 128 - 1
+            elif event_code == controller.EventCode.LEFT_JOYSTICK_Y:
+                velocity.y = -(event.value / 128 - 1)
+            elif event_code == controller.EventCode.RIGHT_JOYSTICK_X:
+                velocity.omega = event.value / 128 - 1
+            elif event_code == controller.EventCode.B_BUTTON and event.value == 1:
+                print('Stopping BWO!')
+                bwo.drive_motors.stop()
+                break
 
-            if event_code in drive_motor_events:
-                if event_code == controller.EventCode.LEFT_JOYSTICK_X:
-                    velocity.x = event.value / 128 - 1
-                elif event_code == controller.EventCode.LEFT_JOYSTICK_Y:
-                    velocity.y = -(event.value / 128 - 1)
-                elif event_code == controller.EventCode.RIGHT_JOYSTICK_X:
-                    velocity.omega = event.value / 128 - 1
-                elif event_code == controller.EventCode.B_BUTTON and event.value == 1:
-                    print('Stopping BWO!')
-                    drive_motors.stop()
-                    break
+            bwo.drive_motors.set_body_velocity(velocity.x, velocity.y, velocity.omega)
 
-                drive_motors.set_body_velocity(velocity.x, velocity.y, velocity.omega)
+        elif event_code in head_events:
+            if event_code == controller.EventCode.RIGHT_JOYSTICK_Y:
+                bwo.head.set_head_position(-(event.value / 128 - 1))
 
-            elif event_code in head_events:
-                if event_code == controller.EventCode.RIGHT_JOYSTICK_Y:
-                    head.set_head_position(-(event.value / 128 - 1))
+
+def main():
+    with Bwo as bwo, get_controller() as controller:
+        if MODE == BwoMode.REMOTE_CONTROL:
+            remote_control(bwo, controller)
+        elif MODE == BwoMode.CAT_SEEKER:
+            cat_seeker(bwo, controller)
 
 
 if __name__ == '__main__':
-    with get_controller() as _controller, Maestro(is_micro=True) as _maestro:
-        main(_controller, _maestro)
+    main()
