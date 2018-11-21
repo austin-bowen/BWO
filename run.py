7584ec50d94ade2f5397a8b56657cf6b4b8b41c8
SaltyHash <austin.bowen.314@gmail.com>
2018-11-20 19:38:41 -0500

    Catch SIGINT and shutdown gracefully.

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
+import signal
 import sys
 
 from bwo.bwo import Bwo, BwoMode
+from bwo.events import send_shutdown_command
 
 
 def main() -> int:
+    def handle_sigint(signal_number, stack_frame):
+        assert signal_number == signal.SIGINT
+        print(f'Received SIGINT; shutting down.')
+        send_shutdown_command()
+
+    signal.signal(signal.SIGINT, handle_sigint)
+
     with Bwo(BwoMode.REMOTE_CONTROL) as bwo:
         bwo.wait_for_shutdown()
 
