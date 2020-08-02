2808ac607799aa5d8c3def965930453595de6edc
Austin Bowen <austin.bowen.314@gmail.com>
2020-08-01 23:30:30 -0400

    Refactor to only use ticks as units, instead of also cm/mm

--- src/arduino/motor_control/motor_control.ino
+++ src/arduino/motor_control/motor_control.ino
@@ -26,16 +26,8 @@
 
 #define LEFT 2
 #define RIGHT 1
-#define SPEED_LIMIT   100
-//#define MIN_MOTOR_PWM 38
-//#define MIN_MOTOR_PWM 2
 #define MIN_MOTOR_PWM 1
 #define MAX_MOTOR_PWM 128
-//#define MAX_MOTOR_PWM 255
-
-// 13 motor shaft ticks * 56 [56:1 gear ratio] * 4 [for some reason] = 2912
-#define ENCODER_PPR  2912
-#define WHEEL_CIR_MM 424
 
 
 typedef signed long ticks_t;
@@ -56,10 +48,11 @@ class Motor {
     int dir_pin;
     volatile ticks_t* ticks_ptr;
 
-    double prev_position;
+    long prev_position;
     unsigned long prev_time;
 
-    // For use by PID controller
+    // For PID controller
+    PID *pid;
     double actual_velocity = 0.0;
     double target_velocity = 0.0;
     double pid_output = 0.0;
@@ -69,16 +62,10 @@ class Motor {
     }
 
     /* Args:
-        motor: LEFT or RIGHT.
-        speed: [-100, 100], which gets scaled to [-MAX_MOTOR_PWM, MAX_MOTOR_PWM].
-    */
-    void SetPwm(double speed) {
-      // Scale and limit the motor speed
-      if (speed > 0) {
-        speed = map(speed, 1, 100, MIN_MOTOR_PWM, MAX_MOTOR_PWM);
-      } else if (speed < 0) {
-        speed = -map(-speed, 1, 100, MIN_MOTOR_PWM, MAX_MOTOR_PWM);
-      }
+     *   speed: [-MAX_MOTOR_PWM, MAX_MOTOR_PWM]
+     */
+    void SetPwm(int speed) {
+      // Limit the motor speed to [-MAX_MOTOR_PWM, MAX_MOTOR_PWM]
       speed = constrain(speed, -MAX_MOTOR_PWM, MAX_MOTOR_PWM);
 
       // Clamp the speed to [-255, 255]
@@ -89,14 +76,14 @@ class Motor {
       digitalWrite(dir_pin, speed < 0);
     }
 
-    /* Sets actual_velocity [cm / s]. */
+    /* Sets actual_velocity [ticks / s]. */
     double UpdateActualVelocity() {
       // Get current state
-      const double position = GetActualPosition();
+      const long position = GetActualPosition();
       const unsigned long time = millis();
-      const double dt = (double)(time - prev_time) / 1000.0;
+      const double dt = (time - prev_time) / 1000.0;
 
-      // Calculate velocity [cm / s]
+      // Calculate velocity [ticks / s]
       actual_velocity = (position - prev_position) / dt;
 
       // Save state
@@ -107,11 +94,14 @@ class Motor {
     }
 
   public:
+    // [ms]
     int sample_time = 1000 / 50;
-    bool debug = true;
-    bool (*forward_check_func)(void) = nullptr;
 
-    PID *pid;
+    // [ticks / s]
+    unsigned long acceleration = 8000;
+
+    bool debug = false;
+    bool (*forward_check_func)(void) = nullptr;
 
     Motor(const int pwm_pin, const int dir_pin, volatile ticks_t* ticks_ptr) {
       this->pwm_pin = pwm_pin;
@@ -121,7 +111,7 @@ class Motor {
       pinMode(pwm_pin, OUTPUT);
       pinMode(dir_pin, OUTPUT);
 
-      this->pid = new PID(&actual_velocity, &pid_output, &target_velocity, 2, 20, 0, DIRECT);
+      this->pid = new PID(&actual_velocity, &pid_output, &target_velocity, 0.05, 0.5, 0, DIRECT);
       pid->SetSampleTime(sample_time);
       pid->SetOutputLimits(0, 0);
       pid->SetMode(MANUAL);
@@ -129,39 +119,44 @@ class Motor {
       Stop();
     }
 
-    /* [cm] */
-    double GetActualPosition() {
-      const static double ticks_to_cm = WHEEL_CIR_MM / (ENCODER_PPR * 10.0);
-      return (*ticks_ptr) * ticks_to_cm;
+    /* [ticks] */
+    long GetActualPosition() {
+      return *ticks_ptr;
     }
 
-    /* [cm / s] */
-    double GetActualVelocity() {
+    /* [ticks / s] */
+    long GetActualVelocity() {
       return actual_velocity;
     }
 
-    void Stop() {
-      SetPwm(0);
-    }
-
-    /* [cm / s] */
+    /* Args:
+     *   velocity: [ticks / s]
+     */
     void SetTargetVelocity(double velocity) {
       // Return if not trying to stop and it's not yet time to re-compute the output
       if (!pid->ReadyToCompute(millis())) {
         return;
       }
 
-      // Make sure we are allowed to move forward
-      if (forward_check_func != nullptr && velocity > 0 && !(*forward_check_func)()) {
+      // If we want to go forward but we can't, set velocity to 0
+      if (velocity > 0 && forward_check_func != nullptr && !(*forward_check_func)()) {
         velocity = 0;
       }
 
       // Update the target and actual velocities
-      target_velocity = velocity;
+      if (acceleration == 0) {
+        target_velocity = velocity;
+      } else {
+        const long dv = (double)acceleration * (double)sample_time / 1000.0;
+        if (abs(velocity - target_velocity) > dv) {
+          target_velocity += velocity > target_velocity ? dv : -dv;
+        } else {
+          target_velocity = velocity;
+        }
+      }
       UpdateActualVelocity();
-      const bool going_wrong_way = (target_velocity * actual_velocity) < 0;
 
-      if (CloseToEqual(target_velocity, 0) || going_wrong_way) {
+      if (CloseToEqual(target_velocity, 0)) {
         if (pid->GetMode() != MANUAL) {
           pid->SetMode(MANUAL);
         }
@@ -174,9 +169,9 @@ class Motor {
         // We don't want the PID controller to tell the motor to go
         // the opposite direction of where it should be going
         if (target_velocity >= 0) {
-          pid->SetOutputLimits(0, 100);
+          pid->SetOutputLimits(0, MAX_MOTOR_PWM);
         } else {
-          pid->SetOutputLimits(-100, 0);
+          pid->SetOutputLimits(-MAX_MOTOR_PWM, 0);
         }
       }
 
@@ -184,13 +179,16 @@ class Motor {
       SetPwm(pid_output);
 
       if (debug) {
-        Serial.println("target_velocity\tactual_velocity\toutput\tposition");
+        Serial.println("target_velocity\tactual_velocity\toutput");
         Serial.print(target_velocity); Serial.print('\t');
         Serial.print(actual_velocity); Serial.print('\t');
         Serial.print(pid_output); Serial.print('\t');
-        Serial.print(GetActualPosition());
         Serial.println();
       }
+    }
+
+    void Stop() {
+      SetPwm(0);
     }
 };
 
@@ -273,36 +271,16 @@ void setup_motors() {
 
 
 void loop() {
-  const static unsigned int LOOP_HZ = 20;
-  const static unsigned int LOOP_MS = 1000 / LOOP_HZ;
-
-  const unsigned long t0 = micros();
-
-  //main2(LOOP_MS);
-  //main3(LOOP_MS);
-  //main4();
   process_command();
-
-  // Delay
-  //const unsigned long proc_ms = (micros() - t0) / 1000;
-  //if (LOOP_MS - proc_ms > 0) {
-  //  delay(LOOP_MS - proc_ms);
-  //}
+  //test_control();
+  //avoid_objects();
 }
 
 
-/* Commands:
-   - set_motor_velocity: byte = 0x11 | left_motor_velocity: signed short | right_motor_velocity: signed short
-     -> ack: byte = 0xAA |
-        actual_left_motor_velocity: signed short |
-        actual_right_motor_velocity: signed short |
-        bumpers: byte = 0b00000lmr
-   - set_pid_parameters: byte = 0x22 | k_p: unsigned char | k_i: unsigned char | k_d: unsigned char
-     -> ack: byte = 0xAA
-*/
 void process_command() {
   const static byte ACK = 0xAA;
   const static byte NCK = 0xFF;
+  const static byte SET_VELOCITY_COMMAND = 0xC0;
 
   static short  left_motor_velocity = 0;
   static short right_motor_velocity = 0;
@@ -314,36 +292,51 @@ void process_command() {
 
   left_motor.SetTargetVelocity(left_motor_velocity);
   right_motor.SetTargetVelocity(right_motor_velocity);
-  left_motor.debug = right_motor.debug = false;
 
   if (!Serial.available()) {
     return;
   }
 
   const byte command = Serial.peek();
-  if (command == 0xC0) {
-    if (Serial.available() < 5) {
+
+  /* "Set Velocity" command structure:
+   * - Recv: 5 bytes total
+   *   - command: byte = 0xC0
+   *   - left_motor_velocity : signed short (MSB first, ticks/s)
+   *   - right_motor_velocity: signed short (MSB first, ticks/s)
+   * - Send: 14 bytes total
+   *   - ACK: byte = 0xAA
+   *   - actual_left_motor_position : signed long  (MSB first, ticks)
+   *   - actual_left_motor_velocity : signed short (MSB first, ticks/s)
+   *   - actual_right_motor_position: signed long  (MSB first, ticks)
+   *   - actual_right_motor_velocity: signed short (MSB first, ticks/s)
+   *   - bumpers: byte (bit 2: left bumper; bit 1: middle bumper; bit 0: right bumper)
+   */
+  if (command == SET_VELOCITY_COMMAND) {
+    // Skip if there are not enough bytes available in the read/write buffers
+    if (Serial.available() < 5 || Serial.availableForWrite() < 14) {
       return;
     }
 
     Serial.read();  // Discard the command
-    left_motor_velocity = (Serial.read() << 8) | Serial.read();
+
+    // Read the target motor velocities
+    left_motor_velocity  = (Serial.read() << 8) | Serial.read();
     right_motor_velocity = (Serial.read() << 8) | Serial.read();
 
-    // Set motor velocities
-    // ...
-
-    // Get motor velocities and bumper states
+    // Get actual motor and bumper states
+    const long   actual_left_motor_position =  left_motor.GetActualPosition();
     const short  actual_left_motor_velocity =  left_motor.GetActualVelocity();
+    const long  actual_right_motor_position = right_motor.GetActualPosition();
     const short actual_right_motor_velocity = right_motor.GetActualVelocity();
     const byte bumpers = left_bumper << 2 | middle_bumper << 1 | right_bumper;
 
     // Send the ACK and states
     Serial.write(ACK);
-    Serial.write(( actual_left_motor_velocity & 0xFF00) >> 8);
-    Serial.write(( actual_left_motor_velocity & 0xFF));
-    Serial.write((actual_right_motor_velocity & 0xFF00) >> 8);
-    Serial.write((actual_right_motor_velocity & 0xFF));
+    Serial_WriteBytes(actual_left_motor_position, 4);
+    Serial_WriteBytes(actual_left_motor_velocity, 2);
+    Serial_WriteBytes(actual_right_motor_position, 4);
+    Serial_WriteBytes(actual_right_motor_velocity, 2);
     Serial.write(bumpers);
 
     last_command_time = millis();
@@ -363,13 +356,20 @@ void process_command() {
 }
 
 
-void main2(const int loop_ms) {
+void Serial_WriteBytes(const long value, const int byte_cnt) {
+  for (int byte_index = byte_cnt - 1; byte_index >= 0; byte_index--) {
+    Serial.write((value >> (byte_index * 8)) & 0xFF);
+  }
+}
+
+
+void avoid_objects() {
   const static int FORWARD_STATE = 0;
   const static int REVERSE_STATE = 1;
   const static int TURN_CW_STATE = 2;
   const static int TURN_CCW_STATE = 3;
 
-  const static int SPEED = 20;
+  const static int SPEED = 1000;
 
   static int state = FORWARD_STATE;
   static unsigned long state_change_time_us = 0;
@@ -440,7 +440,7 @@ void main2(const int loop_ms) {
 }
 
 
-void main3(const int loop_ms) {
+void test_control() {
   static ticks_t user_target_speed = 0;
 
   if (Serial.available()) {
@@ -464,39 +464,10 @@ void main3(const int loop_ms) {
   right_motor.SetTargetVelocity(constrain(e, -speed_limit, speed_limit));
   */
 
+  left_motor.debug = true;
   left_motor.SetTargetVelocity(user_target_speed);
+  left_motor.debug = false;
   //right_motor.SetTargetVelocity(user_target_speed);
-  right_motor.debug = false;
-}
-
-
-void main4() {
-  static ticks_t target = 0;
-
-  if (Serial.available()) {
-    target = Serial.parseInt();
-    while (Serial.available()) {
-      Serial.read();
-    }
-  }
-
-  const double gain = 3;
-  const double limit = 2;
-  const double speed_limit = 50;
-
-  double e = gain * (target - left_motor.GetActualPosition());
-  if (abs(e) < limit) e = 0;
-  left_motor.SetTargetVelocity(constrain(e, -speed_limit, speed_limit));
-
-  e = gain * (target - right_motor.GetActualPosition());
-  if (abs(e) < limit) e = 0;
-  right_motor.SetTargetVelocity(constrain(e, -speed_limit, speed_limit));
-}
-
-
-/* Converts encoder ticks to wheel rotation [mm]. */
-int ticks_to_mm(const ticks_t ticks) {
-  return ticks * WHEEL_CIR_MM / ENCODER_PPR;
 }
 
 
