9d3b8e21723483f7e2e78eda65abedf17eb5716b
Austin Bowen <austin.bowen.314@gmail.com>
2020-08-08 17:12:41 -0400

    Add main.py, which currently has a basic waypoint following algorithm

---
+++ src/python/main.py
@@ -0,0 +1,89 @@
+import itertools
+import time
+from math import atan2, sqrt, pi
+
+from drive_motor_control import DriveMotorController
+from orientation import Orientation
+from utils import normalize_angle_degrees
+
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return f'[x={self.x:.1f}, y={self.y:.1f}]'
+
+    def angle(self, other: 'Point') -> float:
+        """:return: The angle [degrees] of the line drawn from this point to the other point."""
+        angle = atan2(other.y - self.y, other.x - self.x) * 180 / pi
+        return normalize_angle_degrees(angle)
+
+    def distance(self, other: 'Point') -> float:
+        """:return: The distance [no units] of this point from the other point."""
+        return sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+
+
+def test_waypoint_following():
+    waypoints = (
+        Point(0, 0),
+        Point(0, 50),
+        Point(50, 50),
+        Point(50, 0),
+        Point(100, 0),
+        Point(100, 50),
+        Point(50, 50),
+        Point(50, 0),
+    )
+    waypoints = itertools.cycle(waypoints)
+
+    max_linear_speed = 30
+    max_angular_speed = 90
+
+    orientation = Orientation()
+
+    with DriveMotorController('/dev/ttyACM5') as drive_motors:
+        drive_motors.state_change_callbacks.add(orientation.drive_motor_state_change_callback)
+
+        waypoint = next(waypoints)
+        while time.sleep(0.2) is None:
+            robot_point = Point(orientation.x, orientation.y)
+
+            # Robot is at the waypoint?
+            if robot_point.distance(waypoint) < 5:
+                # Move on to the next waypoint!
+                waypoint = next(waypoints)
+
+            # Robot heading is "too far off" from pointing at the waypoint?
+            heading_error = normalize_angle_degrees(robot_point.angle(waypoint) - orientation.heading)
+            if abs(heading_error) > 10:
+                v = 0
+                w = heading_error
+            # Robot is pointing at the waypoint?
+            else:
+                v = robot_point.distance(waypoint)
+                w = heading_error
+
+            # Cap the velocities
+            v = min(max(v, 0), max_linear_speed)
+            w = min(max(w, -max_angular_speed), max_angular_speed)
+
+            print(f'Waypoint: {waypoint}  Orientation: {orientation}')
+
+            drive_motors.set_velocity_unicycle(v, w)
+
+
+def main() -> int:
+    try:
+        test_waypoint_following()
+    except KeyboardInterrupt:
+        print()
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+
+    sys.exit(main())
