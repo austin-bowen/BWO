8d07505f63db74173e3387315874b846abaa6bed
Austin Bowen <austin.bowen.314@gmail.com>
2020-08-16 14:41:26 -0400

    Add helper functions for identifying serial devices

--- src/python/utils.py
+++ src/python/utils.py
@@ -1,3 +1,33 @@
+from typing import Optional
+
+from serial.tools.list_ports import comports
+from serial.tools.list_ports_common import ListPortInfo
+
+
+def get_serial_port_with_hwid(hwid: str) -> Optional[ListPortInfo]:
+    """
+    :return: The first serial port (ListPortInfo instance) with a matching hwid, or None if not found.
+    """
+
+    for serial_port in comports():
+        if serial_port.hwid == hwid:
+            return serial_port
+
+    return None
+
+
+def print_serial_ports() -> None:
+    print('Serial Ports:')
+
+    ports = comports()
+    if not ports:
+        print('[None]')
+        return
+
+    for p in sorted(ports):
+        print(f'name={p.name!r}, device={p.device!r}, description={p.description!r}, hwid={p.hwid!r}')
+
+
 def normalize_angle_degrees(angle):
     """
     :return: The angle normalized to (-180, 180] degrees.
