b502fd775230d3174e99f984946ba7f6410d3d03
Austin Bowen <austin.bowen.314@gmail.com>
2020-08-16 14:41:50 -0400

    Add drive motor and servo control test

--- src/python/main.py
+++ src/python/main.py
@@ -2,9 +2,11 @@ import itertools
 import time
 from math import atan2, sqrt, pi
 
-from drive_motor_control import DriveMotorController
+import gamesir
+from drive_motor_control import DriveMotorController, ticks_to_distance, differential_to_unicycle
+from maestro import MicroMaestro
 from orientation import Orientation
-from utils import normalize_angle_degrees
+from utils import normalize_angle_degrees, get_serial_port_with_hwid
 
 
 class Point:
@@ -23,6 +25,74 @@ class Point:
     def distance(self, other: 'Point') -> float:
         """:return: The distance [no units] of this point from the other point."""
         return sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+
+
+def test_remote_control():
+    drive_motors_serial_port = get_serial_port_with_hwid(
+        'USB VID:PID=2886:802F SER=46EB557A50533050352E3120FF121E27 LOCATION=2-3.1:1.0')
+    servo_control_serial_port = get_serial_port_with_hwid(
+        'USB VID:PID=1FFB:0089 SER=00233827 LOCATION=2-3.3:1.0')
+
+    normal_speed = 40
+    turbo_speed = 80
+    turbo = False
+
+    with gamesir.get_controllers()[0] as controller, \
+            DriveMotorController(controller_serial_port=drive_motors_serial_port.device) as drive_motors, \
+            MicroMaestro(tty=servo_control_serial_port.device) as servo_control:
+
+        prev_state = drive_motors.set_velocity_differential(0, 0)
+
+        v_scale = w_scale = 0
+        for event in controller.read_loop():
+            if event.type not in controller.EVENT_TYPES:
+                continue
+
+            event_code = controller.EventCode(event.code)
+
+            if event_code == controller.EventCode.LEFT_JOYSTICK_X:
+                # Scale from [255, 0] to [-1., 1.]
+                w_scale = (128 - event.value) / 128
+
+            elif event_code == controller.EventCode.LEFT_JOYSTICK_Y:
+                # Scale from [255, 0] to [-1., 1.]
+                v_scale = (128 - event.value) / 128
+
+            elif event_code == controller.EventCode.RIGHT_JOYSTICK_X:
+                value = (128 - event.value) / 128
+                target = 1510 + 500 * value
+                servo_control.set_target(1, target)
+                continue
+
+            elif event_code == controller.EventCode.RIGHT_JOYSTICK_Y:
+                value = (128 - event.value) / 128
+                target = 1500 - 500 * value
+                servo_control.set_target(0, target)
+                continue
+
+            elif event_code == controller.EventCode.RIGHT_TRIGGER_PRESSURE:
+                turbo = event.value >= 200
+
+            else:
+                continue
+
+            max_speed = turbo_speed if turbo else normal_speed
+
+            v = max_speed * v_scale
+            w = 90 * w_scale
+            print(f'Desired:  v:{v:6.1f}  w:{w:6.1f}')
+
+            state = drive_motors.set_velocity_unicycle(v, w)
+
+            dt = state.timestamp - prev_state.timestamp
+            d_left_motor_position = ticks_to_distance(state.left_motor_position - prev_state.left_motor_position)
+            d_right_motor_position = ticks_to_distance(state.right_motor_position - prev_state.right_motor_position)
+
+            v, w = differential_to_unicycle(d_left_motor_position / dt, d_right_motor_position / dt)
+
+            print(f'Actual :  v:{v:6.1f}  w:{w:6.1f}')
+
+            prev_state = state
 
 
 def test_waypoint_following():
@@ -84,7 +154,8 @@ def test_waypoint_following():
 
 def main() -> int:
     try:
-        test_waypoint_following()
+        test_remote_control()
+        # test_waypoint_following()
     except KeyboardInterrupt:
         print()
 
