f8d0ffe9487d206f763274f6a7f9f84b693d718c
Austin Bowen <austin.bowen.314@gmail.com>
2021-07-18 23:19:05 -0400

    Add motor_safety node

--- src/ros2/src/bwo/launch/bwo.launch.py
+++ src/ros2/src/bwo/launch/bwo.launch.py
@@ -4,25 +4,29 @@ from launch_ros.actions import Node
 
 def generate_launch_description() -> LaunchDescription:
     return LaunchDescription([
-        #Node(
-        #    package='realsense_ros2_camera',
-        #    node_executable='realsense_ros2_camera',
-        #),
-        #Node(
-        #    package='bwo',
-        #    node_executable='object_detect',
-        #),
+        Node(
+            package='realsense_ros2_camera',
+            node_executable='realsense_ros2_camera',
+        ),
+        Node(
+            package='bwo',
+            node_executable='object_detect',
+        ),
         Node(
             package='bwo',
             node_executable='follow',
         ),
-        Node(
-            package='bwo',
-            node_executable='bounce',
-        ),
+        #Node(
+        #    package='bwo',
+        #    node_executable='bounce',
+        #),
         Node(
             package='bwo',
             node_executable='neck',
+        ),
+        Node(
+            package='bwo',
+            node_executable='motor_safety',
         ),
         Node(
             package='bwo',
